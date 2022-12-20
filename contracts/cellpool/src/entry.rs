be51e3615aa13336e664755ad482677726d987b4
ian <ian@cryptape.com>
2022-12-20 23:58:30 +0800

    load input, output and witness

--- contracts/cellpool/src/entry.rs
+++ contracts/cellpool/src/entry.rs
@@ -8,19 +8,31 @@ use alloc::{vec, vec::Vec};
 // Import CKB syscalls and structures
 // https://docs.rs/ckb-std/
 use ckb_std::{
+    ckb_constants::Source,
+    ckb_types::{bytes::Bytes, packed::*, prelude::*},
     debug,
-    high_level::{load_script, load_tx_hash},
-    ckb_types::{bytes::Bytes, prelude::*},
+    high_level::{load_cell_data, load_cell_lock, load_script, load_witness_args},
 };
 
 use crate::error::Error;
 
-use ark_serialize::*;
 use ark_bls12_381::{Bls12_381, Fr};
 use ark_groth16::Groth16;
+use ark_serialize::*;
 use ark_snark::SNARK;
 
 type VerifyingKey = <Groth16<Bls12_381> as SNARK<Fr>>::VerifyingKey;
+
+fn load_final_root(script: &Script) -> Result<Vec<u8>, Error> {
+    for i in 0.. {
+        let lock = load_cell_lock(i, Source::Output)?;
+        if lock.as_slice() == script.as_slice() {
+            return load_cell_data(i, Source::Output).map_err(Into::into);
+        }
+    }
+
+    Err(Error::NoFinalRoot)
+}
 
 // args
 // - vk: VerifyingKey<Bls12_381> https://github.com/arkworks-rs/groth16/blob/3464e7910093723481fb98326b040025c5669b58/src/data_structures.rs#L31
@@ -31,7 +43,7 @@ type VerifyingKey = <Groth16<Bls12_381> as SNARK<Fr>>::VerifyingKey;
 pub fn main() -> Result<(), Error> {
     let script = load_script()?;
     let args: Bytes = script.args().unpack();
-    debug!("script args is {:?}", args);
+    debug!("script args len is {:?}", args.len());
 
     // return an error if args is invalid
     if args.is_empty() {
@@ -42,8 +54,11 @@ pub fn main() -> Result<(), Error> {
     let vk = VerifyingKey::deserialize_uncompressed(&*args).unwrap();
 
     // 2. Setup inputs from input / output cell data and witness
+    let initial_root = load_cell_data(0, Source::GroupInput)?;
+    let final_root = load_final_root(&script)?;
+    let witness_args = load_witness_args(0, Source::Input).unwrap();
+
     // 3. Run Groth16 to verify
 
     Ok(())
 }
-
